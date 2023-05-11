a4e5bb6827c29f3a701f1a2f99917c8e5b00cc7d
Fizruk02 <fizrukvush@mail.com>
2023-05-11 19:50:13 +0300

    Dima commits 1

---
+++ laba6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int SIZE = 3;
+    int arr[SIZE][SIZE] = {{-23, -47, -12},
+                          {-34, -23, -56},
+                          {-72, -43, -67}};
+    cout << "Задача 2: ";
+    int sum = arr[SIZE-1][SIZE-1], b = 0;
+    
+    for(int a = 1; a < SIZE; a++){
+        for(int k = 0; k < SIZE-a; k++) b += arr[k][a+k];
+        if(b > sum) sum = b;
+        b = 0;
+    }
+    
+    for(int a = 1; a < SIZE; a++){
+        for(int k = 0; k < SIZE-a; k++) b += arr[a+k][k];
+        if(b > sum) sum = b;
+        b = 0;
+    }
+    cout << sum;
+
+    return 0;
+}
